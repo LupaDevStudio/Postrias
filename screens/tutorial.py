353367fad6cef535baf9c7b9fcda175e03711820
Paul <lupa.dev.studio@gmail.com>
2023-11-27 18:16:09 +0100

    add image on the tutorial screen

--- screens/tutorial.py
+++ screens/tutorial.py
@@ -6,6 +6,8 @@ Module for the tutorial menu.
 ### Imports ###
 ###############
 
+from kivy.properties import StringProperty
+
 from tools.path import (
     PATH_IMAGES,
     PATH_TEXT_FONT
@@ -22,15 +24,18 @@ from tools.constants import (
 ### Class ###
 #############
 
+
 class TutorialScreen(ImprovedScreen):
+
+    counter_tutorial = 0
+    path_tutorial_image = StringProperty()
 
     def __init__(self, **kw):
         super().__init__(
             back_image_path=PATH_IMAGES + "tutorial_background.jpg",
             font_name=PATH_TEXT_FONT,
             **kw)
-
-    counter_tutorial = 0
+        self.path_tutorial_image = PATH_IMAGES + "ending_paleo_max.jpg"
 
     def on_enter(self, *args):
         self.ids.tutorial_text.text = TEXT.tutorial[self.counter_tutorial]
@@ -51,7 +56,7 @@ class TutorialScreen(ImprovedScreen):
         # Go to main menu at the beginning
         if self.counter_tutorial == 0:
             self.manager.current = "menu"
-        
+
         else:
             self.counter_tutorial -= 1
             self.ids.tutorial_text.text = TEXT.tutorial[self.counter_tutorial]
@@ -82,6 +87,6 @@ class TutorialScreen(ImprovedScreen):
 
             else:
                 self.manager.current = "menu"
-        
+
         else:
             self.ids.tutorial_text.text = TEXT.tutorial[self.counter_tutorial]
