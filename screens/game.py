c7ac9add44430762336e08fae2b4ec9d1d4bf462
Agathe <lupa.dev.studio@gmail.com>
2023-11-28 10:09:19 +0100

    Add static add => just need to test it on phone

--- screens/game.py
+++ screens/game.py
@@ -48,6 +48,7 @@ from tools.constants import (
 if platform == "android":
     from kivads import (
         RewardedInterstitial,
+        InterstitialAd,
         TestID
     )
 
@@ -213,6 +214,9 @@ class GameScreen(ImprovedScreen):
             self.reward_interstitial = RewardedInterstitial(
                 REWARD_INTERSTITIAL, self.schedule_reward
             )
+            self.reward_static = InterstitialAd(
+                TestID.INTERSTITIAL
+            )
 
         # Allocate the number of credits
         self.credit = 1
@@ -372,6 +376,8 @@ class GameScreen(ImprovedScreen):
         """
         Called after the ad to continue the game.
         """
+        if platform == "android":
+            self.reward_static.show()
         self.credit = self.credit - 1
         game.continue_game()
         self.update_display_resources()
@@ -380,4 +386,7 @@ class GameScreen(ImprovedScreen):
         if platform == "android":
             self.reward_interstitial = RewardedInterstitial(
                 REWARD_INTERSTITIAL, self.schedule_reward
+            )
+            self.reward_static = InterstitialAd(
+                TestID.INTERSTITIAL
             )
